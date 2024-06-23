7eac24a6cc765d7a8252996f108d1c78cf3bdbcb
Kingkaspev <kingsleychiboy22@gmail.com>
2024-06-23 13:47:06 +0100

    Section 5: CryptographyApp implementation completed succesfully

---
+++ Assesssment/CryptographyApp/AesEncryption.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptographyApp
+{
+    public class AesEncryption
+    {
+        private static readonly string key = "m+TP+9B2iFnE0q6ZRmDZlLqDn7uGzt1CjmzYZHjfn9c=";
+        private static readonly string iv = "pW5TygTwT82MKePfyH8Tlw==";
+
+        public static string Encrypt(string plainText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assesssment/CryptographyApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CryptographyApp;
+
+string original = "Here is some data to encrypt!";
+Console.WriteLine("Original:   {0}", original);
+
+string encrypted = AesEncryption.Encrypt(original);
+Console.WriteLine("Encrypted: {0}", encrypted);
+
+string decrypted = AesEncryption.Decrypt(encrypted);
+Console.WriteLine("Decrypted: {0}", decrypted);
